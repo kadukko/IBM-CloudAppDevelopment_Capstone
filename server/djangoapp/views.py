ef037928f7ac2343203d7a188ab862209674764e
Ricardo <ricardosouza_defn@outlook.com>
2023-10-27 00:42:03 -0400

    Login / logout view

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -32,12 +32,34 @@ def contact(request):
         return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    if request.user.is_authenticated:
+        return redirect('/djangoapp')
+
+    error = None
+
+    if request.method == 'POST':
+        try:
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/djangoapp')
+            else:
+                error = 'INVALID_LOGIN'
+        except:
+            error = 'LOGIN_FAILED'
+
+    context = { "error": error }
+    return render(request, 'djangoapp/login.html', context)    
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    if request.user.is_authenticated:
+        logout(request)
+
+    return redirect('/djangoapp')    
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
